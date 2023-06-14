077da400f318441e4181f63d214b78bddc51af3f
krishgupta1 <krishgupta11110@gmail.com>
2023-06-14 07:48:58 +0530

    B-D-1_Lec2 Done

---
+++ controllers/createTodo.js
@@ -0,0 +1,31 @@
+// Import the model
+
+const Todo = require("../models/Todo.js");
+
+// define route handler
+
+exports.createTodo = async(req, res) => {
+    try{
+        // extract title and descriptions from request body 
+        const {title, description} = req.body;
+        // Create a new Todo Object and insert in DB
+        const response = await Todo.create({title,description});
+        // Send a JSON response with a success flag
+        res.status(200).json(
+            {
+                success:true,
+                data:response,
+                message:"Entry Created Successfully"
+            }
+        );
+    }
+    catch(err){
+        console.log(err);
+        console.log(err);
+        res.status(500).json({
+            success:false,
+            data:"Internal Server Error",
+            message:err.message, 
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000, () =>{
-    console.log("App is Running Successfully")
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+// middleware to pass JSON request body
+app.use(express.json());
+
+// import routes from TodoApi
+const todoRoutes = require("./routes/todos");
+const { createTodo } = require("./controllers/createTodo");
+
+// mount the todo API route
+app.use("/api/v1",todoRoutes);
+
+// start server
+
+app.listen(PORT, () => {
+    console.log(`Server Started Successfully at ${PORT}`);
+})
+
+// Connection to the Database
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+// Default Routes
+app.get("/", (req,res) => {
+    res.send(`<h1>This is HomePage Baby.</h1>`);
 })
